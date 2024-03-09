21aa2e3f4608a8ffc8470104ae420b994deda520
naveed <nchaudhry@gmail.com>
2024-03-10 00:37:16 +0500

    chapter 3: introducing lists practice

--- python-lists.py
+++ python-lists.py
@@ -1,1 +1,50 @@
 print("This file is used for list practice")
+bicycles = ['honda', 'yamaha', 'kawasaki']
+print(bicycles)
+print(bicycles[0])
+
+#Practice some string methods
+#Change first element
+bicycles[0] = 'ducati'
+
+message = f"I like {bicycles[0].title()} bikes"
+print(message)
+
+bicycles.append('vespa')
+
+print(bicycles)
+
+bicycles.insert(1, 'eagle')
+
+print (bicycles)
+
+#print last element of list
+print(bicycles[-1])
+
+
+bicycles.pop()
+print(bicycles.pop())
+del bicycles[0]
+
+print(bicycles)
+
+bicycles.remove('eagle')
+print(bicycles)
+
+#add some more elements in the list 
+
+bicycles.append('Sohrab')
+bicycles.append('China Bike')
+
+print(bicycles)
+bicycles.sort(reverse=True)
+print(bicycles)
+
+sortedList = sorted(bicycles)
+print(sortedList)
+print(bicycles)
+
+bicycles.reverse()
+print(bicycles)
+
+print(len(bicycles))
