f42c2c9ab568df17b49e063364a2d5a4e95ca8a7
naveed <nchaudhry@gmail.com>
2024-03-25 15:20:46 +0500

    Run the game in fullscreen mode

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -6,7 +6,10 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+        self.moving_right = False
+        self.moving_left = False
 
         #Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,7 +18,21 @@ class Ship:
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
+
+
+
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
