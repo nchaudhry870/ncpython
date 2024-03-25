866b5b7addff9453489a368cb71f2bbe8f6424c3
naveed <nchaudhry@gmail.com>
2024-03-25 14:05:36 +0500

    Draw ship on the screen and refactor the code

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Class to manage game assets and behavior """
@@ -10,22 +11,36 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
-        
-        self.screen.fill(self.settings.bg_color)
         self.clock = pygame.time.Clock()
+
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            #Watch for keyboard and mouse events
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            
+
+            
+            self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keyboard and mouse events"""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
-            self.clock.tick()
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen"""
+        #Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #make a game instance and run the game
